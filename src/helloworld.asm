93f6e81f8d56eb034117d2eb14a2cd89a337d179
Nicolás A. Ortega <deathsbreed@themusicinnoise.net>
2016-09-21 10:36:46 +0200

    Use movq instead of just mov.

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -8,12 +8,12 @@ hello_end:
     .global _start
 
 _start:
-    mov $1, %rax                    # Define write syscall
-    mov $1, %rdi                    # Define stdout
-    mov $hello, %rsi                # Give string
-    mov $hello_len, %rdx                   # Give size of the string
+    movq $1, %rax                   # Define write syscall
+    movq $1, %rdi                   # Define stdout
+    movq $hello, %rsi               # Give string
+    movq $hello_len, %rdx           # Give size of the string
     syscall                         # Call the kernel to do work for us
 
-    mov $60, %rax                   # Define exit syscall
-    mov $0, %rdi                    # Give return value
+    movq $60, %rax                  # Define exit syscall
+    movq $0, %rdi                   # Give return value
     syscall                         # Call the kernel to do work for us
